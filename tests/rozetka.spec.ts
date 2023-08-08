db119e6e41ead9c0449039aed4e426ed04b4c880
Tania Shevchuk <96tania96@gmail.com>
2023-08-08 08:57:38 +0300

    Add Email field validation test

--- tests/rozetka.spec.ts
+++ tests/rozetka.spec.ts
@@ -1,123 +1,118 @@
-import {test,expect} from '@playwright/test'
+import {test,expect, Page} from '@playwright/test'
 
 //It would be a positive login case if I knew how to pass the captcha(
-test("logInFormCapchaError", async({page}) => {
+test("Captcha failed during login", async({page}) => {
     await page.goto("https://rozetka.com.ua/ua/");
     expect(page).toHaveTitle("Інтернет-магазин ROZETKA™: офіційний сайт найпопулярнішого онлайн-гіпермаркету в Україні");
     
     await page.click('.header-actions__item.header-actions__item--user');
-    const headerText = await page.textContent('h3.modal__heading');
-    expect(headerText).toEqual(" Вхід");
+    const loginFormHeaderText = await page.textContent('h3.modal__heading');
+    expect(loginFormHeaderText).toEqual(" Вхід");
     
-    await page.click('[id="auth_email"]');
-    await page.type('[id="auth_email"]',"test12@gmail.com");
-    await page.click('[id="auth_pass"]');
-    await page.type('[id="auth_pass"]',"test12");
+    await page.click('#auth_email');
+    await page.type('#auth_email',"test12@gmail.com");
+    await page.click('#auth_pass');
+    await page.type('#auth_pass',"test12");
     await page.keyboard.press("Enter", {"delay":100});
    
-    const errorText = await page.textContent('.error-message_color_red');
-    expect(errorText).toEqual("Необхідно підтвердити, що ви не робот");
+    const captchaErrorText = await page.textContent('.error-message_color_red');
+    expect(captchaErrorText).toEqual("Необхідно підтвердити, що ви не робот");
    
 });
 
 //Check if the password is shown in the login form after clicking the "Show password" button 
-test("loginFormShowPasswordButton", async({page}) => {
+test("ShowPassword button displays a password", async({page}) => {
     await page.goto("https://rozetka.com.ua/ua/");
     await page.click('.header-actions__item.header-actions__item--user');
 
-    await page.click('[id="auth_pass"]');
-    await page.type('[id="auth_pass"]',"QATest");
+    await page.click('#auth_pass');
+    await page.type('#auth_pass',"QATest");
 
-    const statusPasswordBefore = await page.getAttribute('#auth_pass', 'type');
-    expect (statusPasswordBefore).toContain('password');
+    const passwordStatusBefore = await page.getAttribute('#auth_pass', 'type');
+    expect (passwordStatusBefore).toContain('password');
 
     await page.click('.button_type_link.form__toggle-password');
 
-    const statusPasswordAfter = await page.getAttribute('#auth_pass', 'type');
-    expect (statusPasswordAfter).toContain('text');
+    const passwordStatusAfter = await page.getAttribute('#auth_pass', 'type');
+    expect (passwordStatusAfter).toContain('text');
 
 });
 
 //Check if the number of visible products in the section is increased on the main page after clicking the "Show more" button 
-test("mainPageShowMoreButton", async({page}) => {
+test("ShowMore button increases visible products", async({page}) => {
     await page.goto("https://rozetka.com.ua/ua/");
 
-    const quantityOfGoodsBefore = await page.$$('section:nth-child(2) .ng-star-inserted .main-goods__cell');
+    const productElementsBefore = await page.$$('section:nth-child(2) .ng-star-inserted .main-goods__cell');
 
     const buttonName = await page.textContent('section:nth-child(2) button:last-child.main-goods__show-more');
-    expect(buttonName).toEqual(' Показати ще ');
+    expect(buttonName?.trim()).toEqual('Показати ще');
     await page.click('section:nth-child(2) button:last-child.main-goods__show-more');
 
-    const quantityOfGoodsAfter = await page.$$('section:nth-child(2) .ng-star-inserted .main-goods__cell');
-    expect(quantityOfGoodsAfter.length).toEqual(quantityOfGoodsBefore.length * 4);
+    const productElementsAfter = await page.$$('section:nth-child(2) .ng-star-inserted .main-goods__cell');
+    expect(productElementsAfter.length).toBeGreaterThan(productElementsBefore.length);
     
 });
 
 //Check if all required elements are presented in the product card on the catalog page
-test("productCardInCatalog", async({page}) => {
-    await page.goto("https://rozetka.com.ua/ua/");
-    await page.click('.menu-categories.menu-categories_type_main li:first-child');
-    const categoryHeader = await page.textContent('.ng-star-inserted .portal__heading');
-    expect(categoryHeader).toEqual("Комп'ютери та ноутбуки");
-    await page.click('rz-dynamic-widgets rz-widget-producer+rz-widget-list .portal-grid.portal-grid_type_1_6 li:first-child');
-    const catalogHeader = await page.textContent('div .ng-star-inserted h1');
-    expect(catalogHeader).toEqual("Ноутбуки");
+test("Product card elements", async({page}) => {
+    await page.goto("https://rozetka.com.ua/ua/notebooks/c80004/");
 
-    const productCard = await page.$$('.catalog-grid .catalog-grid__cell:first-child .goods-tile__inner>div, .catalog-grid__cell:first-child .goods-tile__inner>a, .catalog-grid__cell:first-child .goods-tile__inner>span');
-    expect(productCard.length).toEqual(10);
+    // const productLableItems = await page.$$('.catalog-grid .catalog-grid__cell:first-child .goods-tile__inner>div, .catalog-grid__cell:first-child .goods-tile__inner>a, .catalog-grid__cell:first-child .goods-tile__inner>span');
+    // expect(productLableItems.length).toEqual(10);
 
-    const productCardItems = await Promise.all(productCard.map(
-        async (productCard)=>{ 
-            const classNames = await productCard.getAttribute("class");
-            const firstClassName = classNames.split(" ")[0];
-            return firstClassName;
+    // const productCardItems = await Promise.all(productLableItems.map(
+    //     async (productCard)=>{ 
+    //         const classNames = await productCard.getAttribute("class");
+    //         if (classNames ===null){
+    //             throw new Error('classNames are not found')
+    //         }
+    //         const firstClassNamesList = classNames.split(" ")[0];
+    //         return firstClassNamesList;
+    //}));
 
-    }));
-    expect(productCardItems).toEqual(["goods-tile__label","goods-tile__actions","goods-tile__picture","goods-tile__colors","goods-tile__heading","goods-tile__rating","goods-tile__prices","goods-tile__availability","goods-tile__promo","goods-tile__hidden-holder"])
+    const productLableItems = await page.$$('.catalog-grid .catalog-grid__cell:first-child .goods-tile__inner>div, .catalog-grid__cell:first-child .goods-tile__inner>a, .catalog-grid__cell:first-child .goods-tile__inner>span');
+    const productCardLableClassNames = await Promise.all(productLableItems.map((card) => card.getAttribute("class")));
+    const firstClassNameList = productCardLableClassNames.map((className)=> className?.split(" ")[0]);
+
+    expect(firstClassNameList).toContain("goods-tile__label");
+    expect(firstClassNameList).toContain("goods-tile__actions");
+    expect(firstClassNameList).toContain("goods-tile__picture");
+    expect(firstClassNameList).toContain("goods-tile__colors");
+    expect(firstClassNameList).toContain("goods-tile__heading");
+    expect(firstClassNameList).toContain("goods-tile__rating");
+    expect(firstClassNameList).toContain("goods-tile__prices");
+    expect(firstClassNameList).toContain("goods-tile__availability");
+    expect(firstClassNameList).toContain("goods-tile__promo");
+    expect(firstClassNameList).toContain("goods-tile__hidden-holder")
+    
 });
 
+
+const insertDataInTheRegistrationForm = async (page:Page,userName:string, userSurname:string,userPhone:string, userPassword:string) => {
+    await page.type('#registerUserName', userName);
+    await page.type('#registerUserSurname', userSurname);
+    await page.type('#registerUserPhone', userPhone);
+    await page.type('#registerUserPassword', userPassword);
+}
 //check the validation of the Email field in the Registration form
-test("checkEmailFieldInRegistrationForm", async ({page}) => {
+test("Email field validation in the Registration form", async ({page}) => {
     await page.goto("https://rozetka.com.ua/ua/");
         
     await page.click('.header-actions__item.header-actions__item--user');
-    const registrationButtonName = await page.innerText('.auth-modal__register-link.button.button--link');
-    expect(registrationButtonName).toEqual("Зареєструватися");
-
     await page.click('.auth-modal__register-link.button.button--link');
     const registrationFormHeader = await page.innerText('h3.modal__heading');
     expect(registrationFormHeader).toEqual("Реєстрація");
 
-    const data = {
-        '#registerUserName': "Тетяна",
-        '#registerUserSurname': "Шевчук",
-        '#registerUserPhone': "09859860",
-        '#registerUserPassword': "12QAtest"
-    };
+    await insertDataInTheRegistrationForm(page, "Тетяна", "Шевчук", "09859860", "12QAtest"); 
 
-    for (const [selector, value] of Object.entries(data)) {
-    await page.type(selector, value);
-    };
-
-    const arrayInvalidData = ["12@gmailcom", "@gmail.com", "куа@gmail.com", "12testgmail.com", "   ", " 12@gmailcom"];
+    const invalidEmailDataList = ["12@gmailcom", "@gmail.com", "куа@gmail.com", "12testgmail.com", "   ", " 12@gmailcom"];
     
-    const errorMess = [];
-    for (const data of arrayInvalidData) {
-    await page.fill('#registerUserEmail', ''); // Очищаємо поле перед вставкою нового значення
+    for (const data of invalidEmailDataList) {
+    await page.fill('#registerUserEmail', ''); 
     await page.type('#registerUserEmail', data);
     await page.click('.button.button--large.button--green.auth-modal__submit');
     const errorMessageText = await page.innerText('.form__row.ng-star-inserted .validation-message.ng-star-inserted');
-    errorMess.push(errorMessageText);
+    expect(errorMessageText).toEqual("Введіть свою ел. пошту")
     }
-    expect(errorMess).toEqual(["Введіть свою ел. пошту", "Введіть свою ел. пошту", "Введіть свою ел. пошту", "Введіть свою ел. пошту", "Введіть свою ел. пошту", "Введіть свою ел. пошту"])
-   
-    // const errorMess = await Promise.all(arrayInvalidData.map(
-    //     async (data)=>{ 
-    //         await page.fill('#registerUserEmail', '');
-    //         await page.type('#registerUserEmail',data);
-    //         await page.click('.button.button--large.button--green.auth-modal__submit');
-    //         const errorMessageText = page.innerText('.form__row.ng-star-inserted .validation-message.ng-star-inserted');
-    //         return errorMessageText;
-    //         }));
 
-})
+});
