bcdd7c247d31a915c81fa70d5d71b9c29d46639c
Tania Shevchuk <96tania96@gmail.com>
2023-09-13 09:55:46 +0300

    Improved utility for working with elementHandle

--- Utilities/Utility.ts
+++ Utilities/Utility.ts
@@ -1,11 +1,24 @@
-import { Page } from "@playwright/test";
+import { Page, ElementHandle } from "@playwright/test";
 
     const utility = {
-     clearAndType: async(page:Page, fieldSelector:string,textToType:string ) =>{
-        await page.click(fieldSelector);
-        await page.type(fieldSelector,"");
-        await page.type(fieldSelector,textToType)
-        }
+    //  clearAndType: async(page:Page, fieldSelector:string,textToType:string ) =>{
+    //     await page.click(fieldSelector);
+    //     await page.type(fieldSelector,"");
+    //     await page.type(fieldSelector,textToType)
+    //     }
+
+        clearAndType: async(page:Page, field: any, textToType:string ) =>{
+           
+            if ( typeof field === "string") {
+                await page.click(field);
+                await page.type(field,"");
+                await page.type(field,textToType)
+                } 
+                else if( typeof field !== "string") {
+                    await field.type("");
+                    await field.type(textToType)
+                }
+}
 }
 
 export {utility}
